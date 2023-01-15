9beedd47e8d206820b4885c0b602dc7acb2c3ca1
jahidcs <csq.jahid@gmail.com>
2023-01-15 14:06:01 +0600

    Dart Crash course: Operators

---
+++ operators/bin/example1.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  print('Operator');
+
+  const age1 = 64.1;
+  const age2 = 30.5;
+  print(age1 + age2);
+  print(age1 + age2 + 30);
+  print(age2 - age1);
+  print(age1 * age2);
+
+  const divided = age1 / age2;
+  print(divided);
+
+  const intDivision = age1 ~/ age2;
+  print(intDivision);
+}
---
+++ operators/bin/example2.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var age = 30;
+  print(--age);
+  print(age);
+  print(++age);
+  print(age);
+  print(age++);
+  print(age);
+  print(~1);
+}
